7f36f8c64c98a755c3b812f7cb7acb86f6a90987
paolino <paolo.veronelli@gmail.com>
2017-06-21 13:17:25 +0200

    more comments

--- TrafficPaolino.hs
+++ TrafficPaolino.hs
@@ -1,4 +1,4 @@
-{-# language TemplateHaskell, OverloadedStrings, OverloadedLists, GeneralizedNewtypeDeriving #-}
+{-# language TemplateHaskell, OverloadedStrings, OverloadedLists, GeneralizedNewtypeDeriving, ViewPatterns#-}
 import  Control.Concurrent      (forkIO, threadDelay)
 import  Control.Concurrent.STM  (atomically, readTChan, writeTChan, newTChanIO)
 import  Control.Monad           (forever, forM_)
@@ -7,7 +7,7 @@ import  Data.Map                (Map, unionWith, (!))
 import  Data.Set                (Set)
 import  Data.Text               (Text)
 import  Data.String             (IsString)
-import  Control.Arrow           (second)
+import  Control.Arrow           (second, (&&&), (***))
 import  Control.Lens            ((%~))
 import  Control.Lens.TH         (makeLenses)
 import  Data.Copointed          (copoint)
@@ -19,53 +19,66 @@ import  Data.Machine    (
                         )
 
 ---------------- types -------------------------------------------------
-
+-- | name of an interface
 newtype Interface       = Interface Text     deriving (Eq,Ord,IsString, Show)
+-- | time as an integer
 newtype Timestamp       = Timestamp Int     deriving (Num,Ord,Eq, Show)
+-- | number of bytes
 newtype Bytes           = Bytes Int         deriving (Num,Ord,Eq, Show)
+-- | ordern number
 newtype OrderNo         = OrderNo Int         deriving (Num,Ord,Eq, Show)
 
-
+-- | partial traffic for each interface
 newtype Traffic         = Traffic (Map Interface Bytes)
+-- | total traffic for each interface
 newtype Usage           = Usage (Map Interface Bytes) deriving (Show)
+-- | order update
 newtype Order           = Order (Map OrderNo (Set Interface)) deriving (Monoid)
+-- | number of bytes for any order (does it make sense?)
 newtype Report          = Report (Map OrderNo Bytes) deriving (Show)
 
+-- | anything tagged with time
 type Timed a = (Timestamp, a)
 
 ----------------------------- library ----------------------------
--- step a moore machine
+-- | step a moore machine feeding  the input 
 stepMoore :: a -> Moore a t -> Moore a t
 stepMoore x (Moore _ f) = f x
 
 ------------ logic ---------------------------------------
--- state of the Mealy machine, we can't keep separate traffic and orders
--- due to cross query for Report
+
+
+-- | state of the Mealy machine.
 data State = State 
-    {   _traffic        :: Moore Traffic Usage --  accumulate traffic
-    ,   _order          :: Order --  actual order
+    {   _traffic        :: Moore Traffic Usage --  ^ accumulated traffic
+    ,   _order          :: Order --  ^ actual order
     }
 
 makeLenses ''State
 
--- step the state given an event
-step :: Either Traffic Order -> State -> State
+-- | type of input for the computation
+type Event = Either Traffic Order
+
+-- | step the state given an event
+step :: Event -> State -> State
 step (Left x) = traffic %~ stepMoore x 
 step (Right s) = order %~ (s `mappend`)
 
--- compute the report 
-mkReport :: Usage -> Order -> Report
-mkReport (Usage b) (Order s) = Report $ (sum . map (b !) . toList) <$> s
+-- | compute a 'Report' from a 'State'
+report :: State -> Report
+report (State (copoint -> Usage u) (Order o)) = Report $ (sum . map (u !) . toList) <$> o
 
--- traffic accumulator machine definition
-keepTraffic :: Moore Traffic Usage
-keepTraffic = flip unfoldMoore mempty $ \u -> (Usage u,\(Traffic u') -> unionWith (+) u u')
 
--- main mealy machine (filter) 
-report :: Mealy (Either Traffic Order) Report
-report = flip unfoldMealy (State keepTraffic mempty) $ \s e -> 
-    let s'@(State t o) = step e s
-    in (mkReport (copoint t) o, s')
+-- | a 'Moore' machine taking a 'Traffic' and exposing the accumulated 'Usage'
+-- we use a Moore machine as the 'Usage' must be sampled on any 'Event'
+-- on 'Traffic' event we update internal Map Interface Bytes and expose it with 'Usage'
+usage :: Moore Traffic Usage
+usage = flip unfoldMoore mempty $ Usage &&& flip (\(Traffic u') -> unionWith (+) u')
+
+-- | a 'Mealy' machine closed on a 'State' which compute a new report on any 'Event'
+-- un each event we step the 'State' and spit out a new 'Report'
+machine :: Mealy Event Report
+machine = unfoldMealy (fmap (report &&& id) . flip step) $ State usage mempty
 
 -------------------- application ----------------------
     
@@ -82,7 +95,7 @@ orders = map (second Order)
     ,   (500, [(1 ,["eth125", "eth899"])])
     ]
 
--- use timestamps to fire events with time relative to start of program * 2000 µs
+-- | use timestamps to simulate arrival of events with time relative to start of program * 2000 µs
 fakeSourcing :: [Timed Traffic] -> [Timed Order] -> IO (MachineT IO k (Timed (Either Traffic Order)))
 fakeSourcing t o = do
     c <- newTChanIO
@@ -92,4 +105,5 @@ fakeSourcing t o = do
 
 main = do
     src <- fakeSourcing samples orders
-    runT $ autoM print <~ (auto $ second report) <~ src
+    -- event time just get through
+    runT $ autoM print <~ (auto $ second machine) <~ src
